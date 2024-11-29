3d6050af580e4610e7db7190021bd3c765102206
ruoton6 <1758220990@qq.com>
2024-11-29 15:22:33 +0800

    更新：侦听器

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-import EventDemo from './components/EventDemo.vue'
-import ComputedDemo from './components/ComputedDemo.vue'
-import ClassDemo from './components/ClassDemo.vue'
+import EventDemo from './components/EventDemo.vue';
+import ComputedDemo from './components/ComputedDemo.vue';
+import ClassDemo from './components/ClassDemo.vue';
+import WatchDemo from './components/WatchDemo.vue';
 </script>
 
 <template>
   <EventDemo />
   <ComputedDemo />
   <ClassDemo />
+  <WatchDemo />
 </template>
---
+++ src/components/WatchDemo.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="watch-wrapper">
+        <div class="watch-title">
+            侦听器
+        </div>
+        <div class="watch-main">
+            <button @click="updateMessage">点击修改文本内容</button>
+            <div class="watch-message">
+                {{ message }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "Hello,World!",
+            flag: true
+        }
+    },
+    // 函数方法
+    methods: {
+        updateMessage() {
+            if (this.flag) {
+                this.message = "你好，世界！"
+                this.flag = false;
+            } else {
+                this.message = "Hello,World!"
+                this.flag = true;
+            }
+        }
+    },
+    // 侦听器
+    watch: {
+        // 这里的侦听器方法名称必须要和响应式数据对象的名称一样
+        // 比如这里侦听的是 data 中的 message 对象
+        message(newValue, oldValue) {
+            console.log("新值：" + newValue);
+            console.log("旧值：" + oldValue);
+        }
+    }
+}
+</script>
+
+<style>
+.watch-wrapper {
+    height: 150px;
+    width: 300px;
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, .5);
+    margin: 10px auto;
+}
+
+.watch-title {
+    height: 50px;
+    width: 100%;
+    background-color: #1e90ff;
+    color: #ffffff;
+    text-align: center;
+    line-height: 50px;
+    font-size: 22px;
+}
+
+.watch-main {
+    height: 100px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+.watch-message {
+    font-size: 18px;
+    font-weight: bold;
+    margin-top: 8px;
+}
+</style>
